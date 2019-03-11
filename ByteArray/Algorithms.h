70c61aacd6fa8297758c4cf3f0394b86ca01031a
Rasmus Bons <rasmusbons@gmail.com>
2019-03-11 10:50:55 +0100

    Added Algorithm.h and Trie.h for implementation

---
+++ ByteArray/Algorithms.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+int ExpBySqurare(int x, int y)
+{
+}
+
+// Binary Search
+
+// Levenshtein Distance 
+
+
+// Binary Expression tree
---
+++ ByteArray/Trie.h
@@ -0,0 +1,9 @@
+#pragma once
+class TrieNode;
+class Trie
+{
+};
+
+class TrieNode
+{
+};
